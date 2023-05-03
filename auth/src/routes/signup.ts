a97b4f934a7ed838ab6ef471b3b7607a26386827
lebel-hounkanrin <lebelhounkanrin@gmail.com>
2023-05-03 15:26:40 +0100

    feat(auth): validate user input

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,13 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("api/users/signup", (req, res) => {
+router.post("api/users/signup", [
+    body("email").isEmail().withMessage("email must be valid"),
+    body("password").trim().isLength({min: 4, max: 20}).withMessage("password lenght must be betwenn 4 and 20")
+], (req: Request, res: Response) => {
+    const {email, password} = req.body
     res.send("signup ...")
 });
 
