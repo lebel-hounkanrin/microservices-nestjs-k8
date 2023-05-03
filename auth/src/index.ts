20a4343d30263bb19ee68777d6f0ed5337c98d8a
lebel-hounkanrin <lebelhounkanrin@gmail.com>
2023-05-03 15:10:36 +0100

    feat(auth): skaffold routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import { json } from "body-parser";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+import { currentUserRouter } from "./routes/currenr-user";
 
 const app = express();
 app.use(json());
+app.use(signinRouter)
+app.use(signupRouter)
+app.use(signoutRouter)
+app.use(currentUserRouter) 
+
 
 app.listen(3000, ()=> {
     console.log("auth app listening on port 3000")
